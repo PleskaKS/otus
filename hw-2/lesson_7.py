9accfda9352325bfa9c60a79ec8b2d4e8414d5ca
PleskaKS <pleskaks@it.mos.ru>
2024-06-24 18:31:20 +0300

    refactoring the description (lesson 7)

--- hw-2/lesson_7.py
+++ hw-2/lesson_7.py
@@ -2,32 +2,52 @@ from enum import Enum
 
 
 class FileType(Enum):
+    """Объявляем класс "FileType", который содержит возможные варианты типов файлов """
     VIDEO = 1
+    """видеофайл"""
     AUDIO = 2
+    """аудиофайл"""
     IMAGE = 3
+    """изображение"""
 
 
 class MediaFile:
+    """Объявляем класс "MediaFile", который будет родтельским и содержит атрибуты,
+    относящиеся к файлу независимо от его типа"""
+
     def __init__(self, file_type, name, size, created, owner, form):
+        """Инициализируем атрибуты, которые будут определять состояние объекта, созданого при помощи конструтора
+        класса """
         self.file_type = file_type
+        """тип файла"""
         self.name = name
+        """имя файла"""
         self.size = size
+        """размер файла"""
         self.created = created
+        """дата создания"""
         self.owner = owner
-        self.form = form
+        """владелец"""
 
 
 class AudioFile(MediaFile):
+    """Класс-наследник от класса "MediaFile", в котором дополнительно инициализируем атрибуты:
+    длительность аудиио файла и аудиоканал"""
 
     def __init__(self, time, file_type, name, size, created, owner, form, channel):
+        """вызываем методы базого класса"""
         super().__init__(file_type, name, size, created, owner, form)
+        """добавляем аргументы, характерные для аудифайла"""
         self.time = time
         self.channel = channel
 
 
 class VideoFile(MediaFile):
+    """Класс-наследник от класса "MediaFile", в котором дополнительно инициализируем атрибуты:
+    разрешение, кодирование, местоположение видеосьемки"""
 
     def __init__(self, file_type, name, size, created, owner, form, resolution, codecs, location=None):
+        """вызываем методы базого класса"""
         super().__init__(file_type, name, size, created, owner, form)
         self.resolution = resolution
         self.codecs = codecs
@@ -35,28 +55,34 @@ class VideoFile(MediaFile):
 
 
 class PhotoFile(MediaFile):
+    """Класс-наследник от класса "MediaFile", в котором дополнительно инициализируем атрибуты:
+    прозрачность, глубина"""
 
     def __init__(self, file_type, name, size, created, owner, form, depth, visibility=None):
         super().__init__(file_type, name, size, created, owner, form)
+        """вызываем методы базого класса"""
         self.visibility = visibility
         self.depth = depth
 
 
 class LocalStorage:
+    """Объявляем класс с методами для локального хранилища с атрибутом путь"""
     def __init__(self, path_file):
         self.path = path_file
 
     def save(self, file):
-        """сохранить файл"""
+        """Этот метод сохраняет файл в локальное хранилище, с аргументом - файл"""
 
     def delete(self, path):
-        """удалить файл"""
+        """Этот метод удаляет файл из локального хранилища, необходимо передать агрумент - путь к файлу"""
 
     def update(self, file, path):
-        """обновить файл"""
+        """Этот метод обновяет файл, принимает аргументы: файл, путь к файлу"""
 
 
 class ConnectStorage:
+    """Объявляем класс для работы с удаленным хранилищем, в инициализуем необходимы для подлючения аргументы:
+    урл, порт, логин и пароль"""
     def __init__(self, url, port, login, password):
         self.url = url
         self.port = port
@@ -64,22 +90,23 @@ class ConnectStorage:
         self.password = password
 
     def connect(self):
-        """создает подкдлючение к удаленному хранилищу"""
+        """Метод создает подкдлючение к удаленному хранилищу"""
 
     def disconect(self):
-        """закрывает подкдлючение к удаленному хранилищу"""
+        """Метод закрывает подкдлючение к удаленному хранилищу"""
 
     def save(self, file, path):
-        """сохранить файл"""
+        """Этот метод позволит сохранить файл в удаленном хранилище, принимает аргументы: сам файл и путь"""
 
     def delete(self, path):
-        """удалить файл"""
+        """Этот метод позволит удалить файл в удаленном хранилище, принимает аргумент: путь к файлу"""
 
     def update(self, file, path):
-        """обновить файл"""
+        """Этот метод обновляет файл в удаленном хранилище, принимает аргументы: файл, путь"""
 
     def convert(self, file_type, path):
-        """конвертировать файл"""
+        """Этот метод конвертирует файл в удаленном хранилище, принимает аргументы: формат, в который надо
+        конвектировать файл и путь к файлу"""
 
 
 class FileConverter:
@@ -142,21 +169,28 @@ class PhotoFileUtils:
 
 
 class FileStorage:
+    """Объявляем класс "FileStorage", который будет содержит функции,
+    относящиеся к хранилищу файлов"""
     def __init__(self, url, port, login, password):
+        """подключение к хранилиу по урлу с логопассом"""
         self.connect = ConnectStorage(url, port, login, password)
         self.connect.connect()
 
     def __del__(self):
+        """разрыв соединения подключения к хранилиу"""
         self.connect.disconect()
 
     def save(self, file, path):
+        """метод позволит сохранить файл в хранилище"""
         prepare_file = self._prepare_file(file)
         self.connect.save(prepare_file, path)
 
     def delete(self, path):
+        """метод позволит удалить файл из хранилища """
         self.connect.delete(path)
 
     def update(self, file, path):
+        """изменить файл в хранилище"""
         prepare_file = self._prepare_file(file)
         self.connect.update(prepare_file, path)
 
